7aa56a02c2b79b0862056e175fb6a3d46b041227
YueWang1996 <YueWang1996@users.noreply.github.com>
2021-03-16 13:52:40 +0100

    Update gen_policy.sh

--- gen_policy.sh
+++ gen_policy.sh
@@ -3,6 +3,6 @@ set -x -e
 # generate a security policy session for python app using template
 unset MRENCLAVE SCONE_CONFIG_ID
 export MRENCLAVE=$(SCONE_HASH=1 python) # get MRENCLAVE/HASH of python
-MRENCLAVE="e1279dcd6acdba6968cb6ac0c1149c1976ed60ecf0ebb3a405d9e1aec658d0a2"
+#MRENCLAVE="e1279dcd6acdba6968cb6ac0c1149c1976ed60ecf0ebb3a405d9e1aec658d0a2"
 envsubst '$MRENCLAVE' < session_template.yml > session.yml
 unset MRENCLAVE SCONE_CONFIG_ID 
