53d7177b5c74877554323a107c08f0727e377faa
YueWang1996 <YueWang1996@users.noreply.github.com>
2021-03-17 21:28:31 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ rm -rf /demo/encrypted_volume/
 mkdir /demo/encrypted_volume/
 
 # execute the python app with the deployed security session, SCONE_CONFIG_ID --> pass remote attestation information to the container
-SCONE_VERSION=7 SCONE_CONFIG_ID=volume_policy/volume_service python
+SCONE_VERSION=7 SCONE_CONFIG_ID=volume_policy/volume_service python3
